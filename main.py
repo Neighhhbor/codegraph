3f40ad37917374a9828daf61a923f860457602ad
Neighhhhbor <1835369528@qq.com>
2024-09-03 10:30:13 +0800

    contains and call(call first)

--- main.py
+++ main.py
@@ -20,13 +20,9 @@ def main():
     code_graph = CodeGraph()
 
     # 遍历整个树形结构并构建图，从根节点开始
-    def build_graph_from_node(node):
-        code_graph.build_graph_from_tree(node)
-        for child in node.children:
-            build_graph_from_node(child)
-
     # 开始从根节点遍历
-    build_graph_from_node(contains_parser.root)
+    # build_graph_from_node(contains_parser.root)
+    code_graph.build_graph_from_tree(contains_parser.root)
 
     # 第二步：解析import关系
     import_parser = ImportParser(config.PROJECT_PATH, repo_name)
@@ -37,12 +33,12 @@ def main():
         code_graph.add_import(importer, imported_module)
 
     # 第三步：解析调用关系
-    # call_parser = CallParser(config.PROJECT_PATH, repo_name)
-    # call_parser.parse()
+    call_parser = CallParser(config.PROJECT_PATH, repo_name)
+    call_parser.parse()
 
     # 处理调用关系
-    # for caller, callee in call_parser.calls:
-    #     code_graph.add_call(caller, callee)
+    for caller, callee in call_parser.calls:
+        code_graph.add_call(caller, callee)
 
     # 最后，将图导入到Neo4j数据库
     neo4j_handler.import_graph(code_graph)
